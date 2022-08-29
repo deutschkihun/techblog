0bbe278b8bda5475e9e4874d9726c4b98fc82c04
KIHUN KIM <kihunkim@KIHUNui-MacBookAir.local>
2022-08-29 15:54:24 +0900

    add oops-prototype post

--- components/intro.tsx
+++ components/intro.tsx
@@ -5,7 +5,7 @@ const Intro = () => {
         techblog.deutschkihun
       </h1>
       <h4 className="text-center md:text-left text-lg mt-5 md:pl-8">
-        A statically generated blog using Next.js and Markdown.
+        A statically generated tech blog using Next.js and Markdown.
       </h4>
     </section>
   );
